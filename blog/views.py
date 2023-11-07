adb070a15203b45a9c1119411cc8f62d1463c490
Timo Hartmann <hartmanntimo@hotmail.de>
2023-11-07 12:16:57 +0000

    Finish db optimization

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 logger =logging.getLogger(__name__)
 
 def index(request):
-    posts = Post.objects.filter(published_at__lte=timezone.now())
+    posts = Post.objects.filter(published_at__lte=timezone.now()).select_related("author")
     logger.debug("Got %d posts", len(posts))
     return render(request, "blog/index.html", {"posts": posts})
 
@@ -32,3 +32,7 @@ def post_detail(request, slug):
   else:
     comment_form = None
   return render(request, "blog/post-detail.html", {"post": post, "comment_form": comment_form})
+
+def get_ip(request):
+  from django.http import HttpResponse
+  return HttpResponse(request.META['REMOTE_ADDR'])
