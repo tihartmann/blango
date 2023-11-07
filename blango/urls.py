6a51dae4d949af378b8a31551b84630736ffe823
Timo Hartmann <hartmanntimo@hotmail.de>
2023-11-07 17:00:54 +0000

    Finish django Allauth

--- blango/urls.py
+++ blango/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path("", blog.views.index),
     path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("allauth.urls")),
     path("accounts/profile/", blango_auth.views.profile, name="profile"),
     path(
       "accounts/register/",
@@ -35,7 +36,7 @@ urlpatterns = [
       name="django_registration_register",
     ),
     path("accounts/", include("django_registration.backends.activation.urls")),
-]
+]   
 
 if settings.DEBUG:
     urlpatterns += [
