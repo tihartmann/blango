c1a072295ee2e47b3617a762564a6788f17bfda3
Timo Hartmann <hartmanntimo@hotmail.de>
2023-11-07 07:44:35 +0000

    Finish custom template tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,16 +1,17 @@
-from django.contrib.auth import get_user_model
 from django import template
 from django.utils.html import format_html
+from blog.models import Post
 
-user_model = get_user_model()
 register = template.Library()
 
-@register.filter
-def author_details(author, current_user=None):
-  if not isinstance(author, user_model):
-    return ""
+@register.simple_tag(takes_context=True)
+def author_details_tag(context):
+  request = context["request"]
+  current_user = request.user
+  post = context["post"]
+  author = post.author
   
-  if author ==current_user:
+  if author == current_user:
     return format_html("<strong>me</strong>")
 
   if author.first_name and author.last_name:
@@ -26,3 +27,26 @@ def author_details(author, current_user=None):
     suffix = ""
 
   return format_html("{}{}{}", prefix, name, suffix)
+
+  
+@register.simple_tag
+def row(extra_classes=""):
+  return format_html('<div class="row {}">', extra_classes)
+
+@register.simple_tag
+def endrow():
+  return format_html("</div>")
+
+@register.simple_tag
+def col(extra_classes=""):
+  return format_html('<div class="col {}">', extra_classes)
+
+@register.simple_tag
+def endcol():
+  return format_html("</div>")
+
+
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(post):
+  posts = Post.objects.exclude(pk=post.pk)[:5]
+  return {"title": "Recent Posts", "posts": posts}
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
 from blog.models import Post
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 
 def index(request):
   posts = Post.objects.filter(published_at__lte=timezone.now())
   return render(request, "blog/index.html", {"posts": posts})
+
+def post_detail(request, slug):
+  post = get_object_or_404(Post, slug=slug)
+  return render(request, "blog/post-detail.html", {"post": post})
