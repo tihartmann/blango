fce6839c7a1fdd96c75929777abf7bcec8ab4fee
Timo Hartmann <hartmanntimo@hotmail.de>
2023-11-08 11:37:40 +0000

    Finish Serializers

--- blango/settings.py
+++ blango/settings.py
@@ -59,6 +59,7 @@ class Dev(Configuration):
         'allauth.account',
         'allauth.socialaccount',
         'allauth.socialaccount.providers.google',
+        'rest_framework',
     ]
 
     SITE_ID = 1
