bdbe1e62da350f752bfb72e85be326906ffd4c18
Timo Hartmann <hartmanntimo@hotmail.de>
2023-11-09 11:36:36 +0000

    Finish browsable API

--- blango/settings.py
+++ blango/settings.py
@@ -61,6 +61,7 @@ class Dev(Configuration):
         'allauth.socialaccount.providers.google',
         'rest_framework',
         'rest_framework.authtoken',
+        'drf_yasg',
     ]
 
     REST_FRAMEWORK = {
@@ -72,6 +73,13 @@ class Dev(Configuration):
             "rest_framework.authentication.SessionAuthentication",
             "rest_framework.authentication.TokenAuthentication",
         ]
+    }
+
+    SWAGGER_SETTINGS = {
+        "SECURITY_DEFINITIONS": {
+            "Token": {"type": "apiKey", "name": "Authorization", "in": "header"},
+            "Basic": {"type": "basic"},
+        }
     }
 
     SITE_ID = 1
