5a10ca15c5a16fdf361d7aae0b00739f639cc171
Timo Hartmann <hartmanntimo@hotmail.de>
2023-11-13 16:02:36 +0000

    Finish caching

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,6 +1,11 @@
 from rest_framework import generics, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_headers, vary_on_cookie
+
+from rest_framework.exceptions import PermissionDenied
 
 from blog.api.serializers import (
   PostSerializer,
@@ -23,10 +28,30 @@ class PostViewSet(viewsets.ModelViewSet):
         
         return PostDetailSerializer
 
+    @method_decorator(cache_page(120))
+    def list(self, *args, **kwargs):
+        return super(PostViewSet, self).list(*args, **kwargs)
+
+    @method_decorator(cache_page(300))
+    @method_decorator(vary_on_headers("Authorization"))
+    @method_decorator(vary_on_cookie)
+    @action(methods=["get"], detail=False, name="Posts by the logged in user")
+    def mine(self, request):
+      if request.user.is_anonymous:
+          raise PermissionDenied("You must be logged in to see which Posts are yours")
+
+      posts = self.get_queryset().filter(author=request.user)
+      serializer =PostSerializer(posts, many=True, context={"request": request})
+      return Response(serializer.data)
+
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     lookup_field = "email"
+
+    @method_decorator(cache_page(300))
+    def get(self, *args, **kwargs):
+        return super(UserDetail, self).get(*args, *kwargs)
 
 
 class TagViewSet(viewsets.ModelViewSet):
@@ -41,3 +66,11 @@ class TagViewSet(viewsets.ModelViewSet):
         )
 
         return Response(post_serializer.data)
+    
+    @method_decorator(cache_page(300))
+    def list(self, *args, **kwargs):
+        return super(TagViewSet, self).list(*args, **kwargs)
+
+    @method_decorator(cache_page(300))
+    def retrieve(self, *args, **kwargs):
+        return super(TagViewSet, self).retrieve(*args, **kwargs)
